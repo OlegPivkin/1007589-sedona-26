417d8be32f66d1ef9b64ddf2ea0eeace14792349
OlegPivkin <44081467+OlegPivkin@users.noreply.github.com>
2019-11-05 01:28:21 +0300

    Fixed comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 (function () {
   var popapSearch = document.querySelector(".search-form");
   var popapBtn = document.querySelector(".map-and-search__btn");
+  var dateIn = popapSearch.querySelector("input[name=date-in]");
+  var dateOut = popapSearch.querySelector("input[name=date-out]");
+
+  popapSearch.addEventListener("submit", function (evt) {
+    if (!dateIn.value || !dateOut.value) {
+      evt.preventDefault();
+      popapSearch.classList.toggle("search-form--error", false);
+      popapSearch.offsetWidth = popapSearch.offsetWidth;
+      popapSearch.classList.add("search-form--error");
+    }
+  });
 
   function onClickPopapBtn(evt) {
     evt.preventDefault();
     popapSearch.classList.toggle("search-form--closed");
-  } 
+  }
 
   popapBtn.addEventListener("click", onClickPopapBtn);
 })();
@@ -20,7 +31,7 @@ function init() {
   }, {
     searchControlProvider: 'yandex#search'
   });
-  
+
   myMap.geoObjects
     .add(new ymaps.Placemark([34.90165716, -111.76700252],{}, {
         preset: 'islands#redDotIconWithCaption'
